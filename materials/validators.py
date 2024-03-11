61dc9a645ab5751fbd4a6cf6b73ea8a613995fc5
Sergo1613 <mentoss2012@gmail.com>
2024-03-11 20:57:09 +0300

    fix DRF_HW_4 close #4

--- materials/validators.py
+++ materials/validators.py
@@ -1,12 +1,12 @@
-import re
+from urllib.parse import urlparse
 
 from rest_framework import serializers
 
 
 def validator_scam_url(value):
-    """ Валидация ссылки на материал """
+    """Валидация ссылки на материал"""
 
-    pattern = re.compile(r'^https?://(?:www\.)?youtube\.com/.*')
     for link in value.split():
-        if not pattern.match(link):
-            raise serializers.ValidationError(f"Invalid YouTube URL: '{link}'")
+        parsed_url = urlparse(link)
+        if parsed_url.netloc != 'youtube.com':
+            raise serializers.ValidationError(f"You are using banned link: '{parsed_url.netloc}', only 'youtube.com'")
